a06e53d8ed8d58ad8d8b41334d8604ce24f08c4d
lee <yeluowoxin@sina.com>
2018-11-01 17:17:11 +0800

    test

---
+++ python/20181031_colletions.py
@@ -0,0 +1,28 @@
+# python容器模块collections
+
+#defaultdict
+from collections import defaultdict
+
+colours = (
+    ('Yasoob', 'Yellow'),
+    ('Ali', 'Blue'),
+    ('Arham', 'Green'),
+    ('Ali', 'Black'),
+    ('Yasoob', 'Red'),
+    ('Ahmed', 'Silver'),
+)
+
+dfdict = defaultdict(list)
+for name, colour in colours:
+  dfdict[name].append(colour)
+
+print(dfdict)
+# defaultdict(<type 'list'>,
+#    {'Arham': ['Green'],
+#     'Yasoob': ['Yellow', 'Red'],
+#     'Ahmed': ['Silver'],
+#     'Ali': ['Blue', 'Black']
+# })
+
+
+
